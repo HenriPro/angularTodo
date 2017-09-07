6cb31629b1e97ab93f07b2b812a9fd32d1dd5788
Henri Pietila <henrismail@gmail.com>
2017-09-08 00:12:54 +0300

    static component

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 angular.module('app', [])
+  .component('todoList', {
+    template: `
+    <h1>Todo List</h1>
+    <input>
+    <button>add</button>
+    <ul>
+      <li></li>
+    </ul>
+    <hr/>
+    <pre><pre>
+    `
+  })
