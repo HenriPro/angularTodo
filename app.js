3ae7c013cf8c9b9c1197af9a88c6f9ee4b88c4bb
Henri Pietila <henrismail@gmail.com>
2017-09-08 00:41:13 +0300

    console logs input on press

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 angular.module('app', [])
   .component('todoList', {
+    controller: function() {
+      this.addTodo = () => {
+        console.log(this.newTodo);
+      }
+    },
     template: `
     <h1>Todo List</h1>
-    <input>
-    <button>add</button>
+    <input ng-model = "$ctrl.newTodo">
+    <button ng-click = "$ctrl.addTodo()">add</button>
     <ul>
       <li></li>
     </ul>
     <hr/>
-    <pre><pre>
+    <pre>{{$ctrl.newTodo}}<pre>
     `
   })
